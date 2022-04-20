6b467cded6142f5088299c8ad0e7c043542d895c
Савва Романов <savva.romanov99@gmail.com>
2022-04-20 15:21:14 +0300

    3

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,14 +5,15 @@ from rest_framework.authtoken import views
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'groups', GroupViewSet)
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
 router.register(r'posts/(?P<post_id>\d+)/comments',
-                CommentViewSet, basename='worstapi')
+                CommentViewSet,
+                basename='comments')
 
 
 urlpatterns = [
-    path('api/v1/', include(router.urls), name='worstapi-list'),
-    path('api/v1/api-token-auth/', views.obtain_auth_token,
-         name='worstapi-detail'),
+    path('v1/', include(router.urls), name='api-list'),
+    path('v1/api-token-auth/', views.obtain_auth_token,
+         name='api-tokenauth'),
 ]
